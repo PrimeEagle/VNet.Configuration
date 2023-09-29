fc456e410db46ed6eab537bb8b7223f9a69d37f5
PrimeEagle <misc@hiddendragon.com>
2023-09-29 21:11:07 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Configuration@1284 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ VNet.Configuration/TooltipAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace VNet.Configuration
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public sealed class TooltipAttribute : Attribute
+    {
+        public string TooltipText { get; }
+
+        public TooltipAttribute(string tooltipText)
+        {
+            TooltipText = tooltipText;
+        }
+    }
+
+}
