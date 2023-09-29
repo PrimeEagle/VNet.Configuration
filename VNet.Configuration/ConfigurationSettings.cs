20e868872b214d1d9d767b4102c92c2c39a7932f
PrimeEagle <misc@hiddendragon.com>
2023-09-29 23:32:26 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Configuration@1286 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ VNet.Configuration/ConfigurationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace VNet.Configuration
+{
+    public class ConfigurationSettings<T> where T : ISettings, new()
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() => new T(), isThreadSafe: true);
+
+        public static T AppSettings => _instance.Value;
+    }
+}
