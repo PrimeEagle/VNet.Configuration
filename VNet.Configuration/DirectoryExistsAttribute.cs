a45592bf48487b01517ea1b1dc6660214ab87f5e
PrimeEagle <misc@hiddendragon.com>
2023-10-02 16:28:08 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Configuration@1310 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.Configuration/DirectoryExistsAttribute.cs
+++ VNet.Configuration/DirectoryExistsAttribute.cs
@@ -4,17 +4,17 @@
 namespace VNet.Configuration
 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class FileExistsAttribute : ValidationAttribute
+    public class DirectoryExistsAttribute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             if (value is not string filePath || string.IsNullOrWhiteSpace(filePath)) return ValidationResult.Success;
-            return !File.Exists(filePath) ? new ValidationResult(FormatErrorMessage(validationContext.DisplayName)) : ValidationResult.Success;
+            return !Directory.Exists(filePath) ? new ValidationResult(FormatErrorMessage(validationContext.DisplayName)) : ValidationResult.Success;
         }
 
         public override string FormatErrorMessage(string name)
         {
-            return $"{name} does not exist as a file on the system.";
+            return $"{name} does not exist as a directory on the system.";
         }
     }
 }
