17cc218c298e0c0c3e73f43f71d11c2878e0a83b
PrimeEagle <misc@hiddendragon.com>
2023-10-02 00:33:44 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Configuration@1302 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.Configuration/PercentageWithPropertiesAttribute.cs
+++ VNet.Configuration/PercentageWithPropertiesAttribute.cs
@@ -7,40 +7,35 @@ namespace VNet.Configuration
     {
         private readonly string[] _comparisonPropertyNames;
 
-        public PercentageWithPropertiesAttribute(string comparisonPropertyNames)
+        // ReSharper disable once ParameterTypeCanBeEnumerable.Local
+        public PercentageWithPropertiesAttribute(string[] comparisonPropertyNames)
         {
-            _comparisonPropertyNames = comparisonPropertyNames.Split(',').Select(s => s.Trim()).ToArray();
+            _comparisonPropertyNames = comparisonPropertyNames.Select(s => s.Trim()).ToArray();
         }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            double dVal;
-            if (!double.TryParse(value.ToString(), out dVal))
+            if (!double.TryParse(value.ToString(), out _))
             {
-                return new ValidationResult($"Property value {value.ToString()} could not be converted to a number.");
+                return new ValidationResult($"Property value {value} could not be converted to a number.");
             }
 
             double sum = 0;
-            for (var i = 0; i < _comparisonPropertyNames.Length; i++)
+            foreach (var comparisonPropertyName in _comparisonPropertyNames)
             {
-                var comparisonPropertyName = _comparisonPropertyNames[i];
                 var comparisonPropertyInfo = validationContext.ObjectType.GetProperty(comparisonPropertyName);
                 if (comparisonPropertyInfo == null)
                     return new ValidationResult($"Property {comparisonPropertyName} not found.");
 
 
                 var comparisonValue = comparisonPropertyInfo.GetValue(validationContext.ObjectInstance);
-                double dCompVal;
-                if (double.TryParse(comparisonValue.ToString(), out dCompVal))
+                if (double.TryParse(comparisonValue?.ToString(), out var dCompVal))
                 {
                     sum += dCompVal;
                 }
             }
 
-            if (Math.Abs(sum - 100d) < double.Epsilon)
-                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
-
-            return ValidationResult.Success;
+            return Math.Abs(sum - 100d) < double.Epsilon ? new ValidationResult(FormatErrorMessage(validationContext.DisplayName)) : ValidationResult.Success;
         }
 
         public override string FormatErrorMessage(string name)
