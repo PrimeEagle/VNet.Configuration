48e7e8618435409eca469dcab3adeb3b78960bbb
PrimeEagle <misc@hiddendragon.com>
2023-10-12 17:04:25 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Configuration@1368 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ VNet.Configuration/RequiredIfFalseAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+#pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+#pragma warning disable CS8603 // Possible null reference return.
+
+namespace VNet.Configuration
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class RequiredIfFalseAttribute : ValidationAttribute
+    {
+        private readonly string _booleanPropertyName;
+
+        public RequiredIfFalseAttribute(string booleanPropertyName)
+        {
+            _booleanPropertyName = booleanPropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var booleanProperty = validationContext.ObjectType.GetProperty(_booleanPropertyName);
+            if (booleanProperty == null)
+            {
+                return new ValidationResult($"Unknown property: {_booleanPropertyName}");
+            }
+
+            var booleanValue = booleanProperty.GetValue(validationContext.ObjectInstance) as bool?;
+
+            if ((booleanValue ?? true) && string.IsNullOrEmpty(Convert.ToString(value)))
+            {
+                return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ VNet.Configuration/RequiredIfTrueAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+#pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+#pragma warning disable CS8603 // Possible null reference return.
+
+namespace VNet.Configuration
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class RequiredIfTrueAttribute : ValidationAttribute
+    {
+        private readonly string _booleanPropertyName;
+
+        public RequiredIfTrueAttribute(string booleanPropertyName)
+        {
+            _booleanPropertyName = booleanPropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var booleanProperty = validationContext.ObjectType.GetProperty(_booleanPropertyName);
+            if (booleanProperty == null)
+            {
+                return new ValidationResult($"Unknown property: {_booleanPropertyName}");
+            }
+
+            var booleanValue = booleanProperty.GetValue(validationContext.ObjectInstance) as bool?;
+
+            if ((booleanValue ?? false) && string.IsNullOrEmpty(Convert.ToString(value)))
+            {
+                return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
