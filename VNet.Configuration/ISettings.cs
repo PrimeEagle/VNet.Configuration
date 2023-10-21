44b46579a3e4027cfd5a9187f2e4ac883c42d159
PrimeEagle <misc@hiddendragon.com>
2023-10-21 02:29:17 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Configuration@1416 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.Configuration/ISettings.cs
+++ VNet.Configuration/ISettings.cs
@@ -1,6 +1,20 @@
-﻿namespace VNet.Configuration
+﻿using System.Collections.ObjectModel;
+
+namespace VNet.Configuration
 {
+    public class TabItemModel
+    {
+        public string Header { get; }
+        public string Content { get; }
+        public TabItemModel(string header, string content)
+        {
+            Header = header;
+            Content = content;
+        }
+    }
+
     public interface ISettings
     {
+
     }
 }
