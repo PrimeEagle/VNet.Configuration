f296e486dacdb3e529f7eb95deea3993bac55f69
PrimeEagle <misc@hiddendragon.com>
2023-10-27 05:43:54 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.Configuration@1470 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ VNet.Configuration/ConfigurationService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace VNet.Configuration
+{
+    public class ConfigurationService : IConfigurationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ConfigurationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public T GetConfiguration<T>() where T : class, new()
+        {
+            using var serviceScope = _serviceProvider.CreateScope();
+            var options = serviceScope.ServiceProvider.GetRequiredService<IOptions<T>>();
+
+            return options.Value;
+        }
+    }
+}
---
+++ VNet.Configuration/IConfigurationService.cs
@@ -0,0 +1,6 @@
+﻿namespace VNet.Configuration;
+
+public interface IConfigurationService
+{
+    T GetConfiguration<T>() where T : class, new();
+}
